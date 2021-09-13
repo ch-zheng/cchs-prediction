fc06df4c2b2278b565394712e013c5eaba7b5a86
Angeli <angeli.mittal@gmail.com>
2021-09-13 11:20:58 -0500

    stratify people samples, labels

--- utility/encode.py
+++ utility/encode.py
@@ -10,11 +10,11 @@ samples = []
 labels = []
 
 # Encodings
-label_encoding = {'control': 0, 'cchs': 1}
-race_encoding = {'other': 0, 'unknown': 0, 'black': 1, 'white': -1}
+# label_encoding = {'control': 0, 'cchs': 1}
+# race_encoding = {'other': 0, 'unknown': 0, 'black': 1, 'white': -1}
 # Angeli's encodings
-# label_encoding = {'CONTROL': 0, 'CCHS': 1}
-# race_encoding = {'Other': 0, 'UNKNOWN': 0, 'African American': 1, 'Caucasian': -1}
+label_encoding = {'CONTROL': 0, 'CCHS': 1}
+race_encoding = {'Other': 0, 'UNKNOWN': 0, 'African American': 1, 'Caucasian': -1}
 age_encoding = {
     '0-1': 0,
     '2-3': 1,
@@ -28,25 +28,26 @@ age_encoding = {
 }
 
 original_pts = list(range(0, 68))
-omitted_pts = [1, 3, 5, 11, 13, 15, 18, 25, 28]
+omitted_pts = []#1, 3, 5, 11, 13, 15, 18, 25, 28]
 
 # Read CSV file
 print('Opening CSV file')
-with open('data/samples.csv') as csv_file:
+with open('data/samples_updated.csv') as csv_file:
     rows = iter(csv.reader(csv_file))
     next(rows) # Skip header row
     count = 0
     for row in rows:
         # Get data
-        label = label_encoding[row[1]]
-        race = race_encoding[row[2]]
-        age = age_encoding[row[3]]
+        subject = row[1]
+        label = label_encoding[row[2]]
+        race = race_encoding[row[3]]
+        age = age_encoding[row[4]]
         landmarks = []
         for i in original_pts:
             if i in omitted_pts: # don't include omitted points!
                 continue
-            landmarks.append(row[4+2*i]) # x
-            landmarks.append(row[5+2*i]) # y
+            landmarks.append(row[5+2*i]) # x
+            landmarks.append(row[6+2*i]) # y
         landmarks = np.array(tuple(map(int, landmarks)), dtype=np.single)
         # Standardize landmarks to [0, 1]
         landmarks_x = landmarks[::2]
@@ -57,10 +58,11 @@ with open('data/samples.csv') as csv_file:
         landmarks[1::2] = landmarks_y
         landmarks = landmarks / landmarks.max()
         # Aggregate features
-        sample = np.empty(2 + landmarks.size, dtype=np.single)
-        sample[0] = race
-        sample[1] = age
-        sample[2:] = landmarks
+        sample = np.empty(3 + landmarks.size, dtype=np.single)
+        sample[0] = subject
+        sample[1] = race
+        sample[2] = age
+        sample[3:] = landmarks
         # Append to result
         samples.append(sample)
         labels.append(label)
@@ -69,7 +71,7 @@ print('Finished reading', count, 'table entries')
 
 # Save as .npy files
 samples = np.stack(samples)
-np.save('data/phase1/samples_phase1.npy', samples)
+np.save('data/stratify_people/samples.npy', samples)
 labels = np.array(labels, dtype=np.single)
-np.save('data/phase1/labels_phase1.npy', labels)
+np.save('data/stratify_people/labels.npy', labels)
 print('Arrays saved')
